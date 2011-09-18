27b42267189b96f90e949e963e60a315d4083561
Lauren Rabaino <lauren.rabaino@gmail.com>
2011-09-18 13:27:27 -0700

    removed hyperlink from title of pages

--- page.php
+++ page.php
@@ -13,7 +13,7 @@
 				
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
 			
-				<h2 class="title_page"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+				<h2 class="title_page"><?php the_title(); ?></h2>
 			
 				<div id="content-main">
 					
